2a84ef7efe53e3ef3ef6cfa2bc66e1102c36e244
arthurevg <arthurevangelista05@gmail.com>
2024-10-25 19:58:04 -0300

    Correcao

--- obj/Debug/net8.0/GerenciadorDeTarefasAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/GerenciadorDeTarefasAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GerenciadorDeTarefasAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e262c790316e044acb3c30955abfdda98561ac9")]
 [assembly: System.Reflection.AssemblyProductAttribute("GerenciadorDeTarefasAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GerenciadorDeTarefasAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
